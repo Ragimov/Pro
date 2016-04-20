18117c041c6e0e400c1055911457ea3cdd826baf
Elmar <relmar@mail.ua>
2016-04-20 21:10:53 +0300

    init

---
+++ src/LAb.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Acer on 20.04.2016.
+ */
+public class LAb {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 2;
+
+        int mul = Multiplear.mul(a,b);
+        System.out.println(mul);
+    }
+}
